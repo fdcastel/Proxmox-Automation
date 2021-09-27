be11eddf50603642655d98c95985e3eef46c3885
F.D.Castel <fdcastel@gmail.com>
2021-09-27 03:00:28 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Proxmox VE automation scripts
+
+Collection of scripts to create VMs in Proxmox VE.
+
+
+
+## How to install
+
+To download all scripts into your temp folder:
+
+```bash
+source <(curl -Ls https://bit.ly/p-v-a)
+```
---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+main () {
+    URL='https://codeload.github.com/fdcastel/PVE-Automation/zip/master'
+
+    TEMP_FOLDER=$(mktemp -d)
+
+    ZIP_FILE="$TEMP_FOLDER/PVE-Automation-master.zip"
+    wget -q -O $ZIP_FILE $URL
+    if [ $? -ne 0 ]; then
+        echo "Error downloading '$URL'."
+        return 1
+    fi
+
+    unzip -q $ZIP_FILE -d $TEMP_FOLDER
+    if [ $? -eq 127 ]; then
+        echo "Command 'unzip' not found. Install it with 'apt-get install unzip -y'."
+        return 1
+    fi
+
+    cd "$TEMP_FOLDER/PVE-Automation-master"
+
+    cat > ./clean.sh <<EOF
+    cd /
+    rm -R ${TEMP_FOLDER}
+EOF
+
+    sleep .5
+    chmod +x ./*.sh
+
+    echo "Ready! Type '. clean.sh' when finished." 
+}
+
+main "$@"
