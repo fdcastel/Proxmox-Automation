ce35130fe00ba6bafc192f97b2d99920e651c33e
F.D.Castel <fdcastel@gmail.com>
2022-12-19 19:53:54 -0300

    bootstrap.sh: Try to use unzip before install. Removes clean.sh. Renames project folder.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
 main () {
-    URL='https://codeload.github.com/fdcastel/PVE-Automation/zip/master'
+    URL='https://codeload.github.com/fdcastel/Proxmox-Automation/zip/master'
 
     TEMP_FOLDER=$(mktemp -d)
 
-    ZIP_FILE="$TEMP_FOLDER/PVE-Automation-master.zip"
+    ZIP_FILE="$TEMP_FOLDER/Proxmox-Automation-master.zip"
     wget -q -O $ZIP_FILE $URL
     if [ $? -ne 0 ]; then
         echo "Error downloading '$URL'."
         return 1
     fi
 
-    # Install unzip
-    apt-get update
-    apt-get -y install unzip
-
     unzip -q $ZIP_FILE -d $TEMP_FOLDER
     if [ $? -eq 127 ]; then
-        echo "Command 'unzip' not found. Install it with 'apt-get install unzip -y'."
-        return 1
+        # Unzip not found. Try to install.
+        apt-get update
+        apt-get -y install unzip
+
+        unzip -q $ZIP_FILE -d $TEMP_FOLDER
+        if [ $? -eq 127 ]; then
+            echo "Command 'unzip' not found. Install it with 'apt-get install unzip -y'."
+            return 1
+        fi
     fi
 
-    cd "$TEMP_FOLDER/PVE-Automation-master"
-
-    cat > ./clean.sh <<EOF
-    cd /
-    rm -R ${TEMP_FOLDER}
-EOF
-
+    cd "$TEMP_FOLDER/Proxmox-Automation-master"
     sleep .5
     chmod +x ./*.sh
-
-    echo "Ready! Type '. clean.sh' when finished." 
 }
 
 main "$@"
